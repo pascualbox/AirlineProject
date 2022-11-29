643a76a99489b50cd74f9e8960ab659c3e44c36f
JoseMartinCalvo <117278127+JoseMartinCalvo@users.noreply.github.com>
2022-11-29 10:13:05 +0100

    Base App
    App generation and base components

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Body from "./components/Body/Body";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
